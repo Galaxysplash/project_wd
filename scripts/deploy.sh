e9c69fbca8c3c483a07070ea4e74ce912bef04ac
Marcel <Marcel.Hajek@gmx.de>
2025-05-31 14:25:30 +0200

    ...

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,15 @@ set -e
 
 BOT_USER=marcel
 BOT_HOST=bot
-WORKSPACE=ros_projects/project_wd
+RUNTIME_PATH=project_wd
 
-rsync -az --delete \
+ssh $BOT_USER@$BOT_HOST "mkdir -p ~/$RUNTIME_PATH"
+
+rsync -az --delete -L \
   ~/ros_projects/project_wd/build \
   ~/ros_projects/project_wd/install \
   ~/ros_projects/project_wd/log \
-  $BOT_USER@$BOT_HOST:~/$WORKSPACE/
+  $BOT_USER@$BOT_HOST:~/$RUNTIME_PATH/
+
+echo "Starting remote node on bot..."
+ssh $BOT_USER@$BOT_HOST "source ~/$RUNTIME_PATH/install/setup.bash && ros2 run genises_node genises_node"
